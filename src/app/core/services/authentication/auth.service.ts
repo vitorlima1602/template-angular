cbba47f8152742a8cebc7fa837d3d3f2c98fc8a5
Vitor Lima <vitor_lima1602@hotmail.com>
2024-07-29 18:03:15 -0300

    Adicionando guards

---
+++ src/app/core/services/authentication/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor() { }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('authToken');
+  }
+
+  login(token: string): void {
+    localStorage.setItem('authToken', token);
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+  }
+}
