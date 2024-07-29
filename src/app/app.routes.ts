6ce09f009f432e3d74433ed073e3ed6af1775726
Vitor Lima <vitor_lima1602@hotmail.com>
2024-07-29 17:15:01 -0300

    Adicao de lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/layout/layout.component').then(m => m.LayoutComponent),
+        children: [
+          {
+            path: 'home',
+            loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+          },
+        //   {
+        //     path: 'about',
+        //     loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)
+        //   }
+        ]
+      },
+];
